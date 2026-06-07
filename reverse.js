04e75280e52a37b96412878e2ad17c9ed3626f64
Aswin Krishna <aswinasus233@gmail.com>
2026-06-07 23:40:47 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,6 @@
+/*1. Reverse an Array Without Using an Additional Array
+Reverse the elements of a given array in-place without using
+any extra array.*/
+
+const car=["BMW","BENZ","SKODA"];
+console.log(car.reverse());
